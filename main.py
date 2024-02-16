acc755388dd913c1ec8d18d9a4d508d0abdfbd0d
sem@frankapc <sem@teknologisk.dk>
2024-02-16 10:55:20 +0100

    made functions and cleaned up in file

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from panda_ros_lib import PandaArm
+from panda_ros_lib import PandaArm, IterativeParabolicTimeParameterization
 import numpy as np
 
 arm = PandaArm()
@@ -9,18 +9,40 @@ def move_to_approx_over_pc():
     pose.position.y = -0.1258678959602396
     pose.position.z= 0.2565689323721724
 
-    arm.move_to_cartesian(pose)
+    arm.move_to_joint(pose)
 
+def find_pc_y():
+    arm.relative_move(2,0.01)
+    arm.relative_move(1,-0.3)
+    arm.relative_move(2, -0.02)
+    pose = arm.get_current_pose()
+    pose.position.y += 0.3
+    arm.move_to_contact(pose)
+
+    current_pose = arm.get_current_pose()
+    pc_y = current_pose.position.y
+    return pc_y
+
+def find_pc_z():
+    arm.align_to_base()
+    result = arm.move_to_contact()
+    if not result[0][2]:
+        print("PANIC!!!")
+        exit()
+
+    current_pose = arm.get_current_pose()
+    pc_z = current_pose.position.z
+    return pc_z
 
 def grasp_tool():
     tool_pose = arm.get_current_pose()
     tool_pose.position.x = 0.5719577898768148
     tool_pose.position.y = 0.33215015088761674
     tool_pose.position.z = 0.30
-    arm.move_to_cartesian(tool_pose)
+    arm.move_to_joint(tool_pose)
     arm.align_to_base()
     arm.gripper.open()
-    tool_pose.position.z = 0.148
+    tool_pose.position.z = 0.145
     arm.move_to_cartesian(tool_pose)
     arm.align_to_base()
     arm.gripper.grasp(0.02, 40)
@@ -33,9 +55,9 @@ def place_tool():
     tool_pose.position.x = 0.5719577898768148
     tool_pose.position.y = 0.33215015088761674
     tool_pose.position.z = 0.30
-    arm.move_to_cartesian(tool_pose)
+    arm.move_to_joint(tool_pose)
     arm.align_to_base()
-    tool_pose.position.z = 0.148
+    tool_pose.position.z = 0.145
     arm.move_to_cartesian(tool_pose)
     arm.gripper.open()
     
@@ -49,7 +71,7 @@ def move_to_approx_over_screen():
     pose.position.z = 0.23626627720711202
     ori = arm.rotate(0,0,-np.pi/2, False)
     pose.orientation = ori
-    arm.move_to_cartesian(pose)
+    arm.move_to_joint(pose)
 
 
 def contact_with_screen_frame():
@@ -58,7 +80,6 @@ def contact_with_screen_frame():
     if not result[0][2]:
         print("PANIC!!!")
         exit()  
-    arm.align_to_base()
     current_pose = arm.get_current_pose()
     current_pose.position.y -= 0.2
     arm.move_to_contact(current_pose)
@@ -91,29 +112,17 @@ def main():
 
     while True:
         move_to_approx_over_pc()
-        arm.align_to_base()
-        result = arm.move_to_contact()
-        if not result[0][2]:
-            print("PANIC!!!")
-            exit()
-
-        current_pose = arm.get_current_pose()
-        pc_z = current_pose.position.z
-
-        arm.relative_move(2,0.01)
-        arm.relative_move(1,-0.3)
-        arm.relative_move(2, -0.02)
-        pose = arm.get_current_pose()
-        pose.position.y += 0.3
-        arm.move_to_contact(pose)
-
-        current_pose = arm.get_current_pose()
-        pc_y = current_pose.position.y
+        pc_z = find_pc_z()
+        pc_y = find_pc_y()
 
         arm.relative_move(1, -0.02)
         arm.move_to_neutral()
 
-        process_remove_screen_frame()
+        grasp_tool()
+        move_to_approx_over_screen()
+        contact_with_screen_frame()
+        remove_screen_frame()
+        place_tool()
 
         arm.move_to_neutral()
 

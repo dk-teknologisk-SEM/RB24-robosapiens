dd4c357c91eebac3dcbaa66a730025ca44d83fdd
sem@frankapc <sem@teknologisk.dk>
2024-02-27 10:14:49 +0100

    deleted not used functions

--- main.py
+++ main.py
@@ -163,18 +163,6 @@ def place_tool(tool_pose):
     arm.relative_move(2, 0.1)
     
 
-def move_to_approx_over_screen():
-    # arm.move_to_neutral()
-    pose = arm.get_current_pose()
-    # neutral as starting position
-    pose.position.x = 0.48081756547124316
-    pose.position.y = 0.0058284339256326895
-    pose.position.z = 0.12026627720711202
-    ori = arm.rotate(0,0,-np.pi/2, False)
-    pose.orientation = ori
-    arm.move_to_joint(pose)
-
-
 def contact_with_screen_frame():
     arm.align_to_base()
     result = arm.move_to_contact()
@@ -200,15 +188,6 @@ def remove_screen_frame():
     current_pose.orientation = arm.rotate(-0.7,0,0, False)
     arm.move_to_cartesian(current_pose)
 
-
-
-def process_remove_screen_frame():
-    grasp_tool()
-    move_to_approx_over_screen()
-    contact_with_screen_frame()
-    remove_screen_frame()
-    place_tool()
-    
 
 def main():
 
